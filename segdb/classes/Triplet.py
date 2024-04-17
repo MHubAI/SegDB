ce8f18b850f804019a6ce2586acede3e1cf1f5b0
LennyN95 <lenny@moontec.de>
2024-04-17 17:46:05 +0200

    extend segment instantiation capabilities and minor fixes
    + convenience functions to find and instaniate segments from id, name and json
    +  factory class method to find and instantiate segments by triplets
    + fix typing and type casting for segment class variables
    + convenience methods to specify the anatomic region of a segment and to specify the property of an existing segment from another segment
    + convenience class mehods to instantiate and find triplets by a code and optional coding scheme designator

--- segdb/classes/Triplet.py
+++ segdb/classes/Triplet.py
@@ -11,18 +11,21 @@ Email:  leonard.nuernberg@maastrichtuniversity.nl
 
 
 from .DB import db
+import pandas as pd
+from typing import Optional
 
 class Triplet:
 
     code: str
     scheme_designator: str
     meaning: str
-
-    def __init__(self, id: str) -> None:
+    
+    @classmethod
+    def _getLookupTable(cls, id: str) -> pd.DataFrame:
         
         # id prefix
         id_prefix = id.split("_")[0]
-
+        
         # select lookup table
         lookup_db = None
         if id_prefix == "T":
@@ -34,6 +37,13 @@ class Triplet:
         else:
             raise ValueError(f"Unknown triplet id prefix: {id_prefix}")
 
+        return lookup_db
+
+    def __init__(self, id: str) -> None:
+        
+        # get loopup table
+        lookup_db = self._getLookupTable(id)
+
         # lookup by id
         data = lookup_db.loc[id]
 
@@ -43,6 +53,24 @@ class Triplet:
         self.scheme_designator = str(data['CodingSchemeDesignator'])
         self.meaning = str(data['CodeMeaning'])
 
+    @classmethod
+    def getByCode(cls, code: int, coding_scheme_designator: str = "SCT") -> 'Triplet':
+        triplet = cls.findByCode(code, coding_scheme_designator)
+        if triplet is None:
+            raise ValueError(f"Triplet with code '{code}' and coding scheme designator '{coding_scheme_designator}' not found.")
+        return triplet
+
+    @classmethod
+    def findByCode(cls, code: int, coding_scheme_designator: str = "SCT") -> Optional['Triplet']:
+        
+        # look into each table for code and coding scheme designator
+        for table in [db.types, db.categories, db.modifiers]:
+            for id, row in table.iterrows():
+                if row['CodeValue'] == code and row['CodingSchemeDesignator'] == coding_scheme_designator:
+                    return cls(str(id))
+                
+        raise ValueError(f"Triplet with code '{code}' and coding scheme designator '{coding_scheme_designator}' not found.")
+        
 
     def __str__(self) -> str:
         return f"{self.id}:{self.scheme_designator}:{self.code}:{self.meaning}"
